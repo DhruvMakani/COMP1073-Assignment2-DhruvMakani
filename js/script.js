c7e1462c053f027c497b5c7992fa95eeee9ac368
DhruvMakani <124004385+DhruvMakani@users.noreply.github.com>
2024-11-10 14:49:47 -0500

    Update Pricing for ingredients, bases, and sizes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,26 @@
 class Smoothie {
-    constructor(ingredients) {
+    constructor(ingredients, base, size) {
         this.ingredients = ingredients;
-    }
+        this.base = base;
+        this.size = size;
+}
+
+// Pricing for ingredients, bases, and sizes
+static prices = {
+    ingredients: {
+        Banana: 1.5,
+        Strawberry: 2.0,
+        Blueberry: 2.0,
+        Mango: 3.0,
+    },
+    bases: {
+        "Almond Milk": 1.0,
+        "Coconut Water": 1.2,
+        "Regular Milk": 0.8,
+    },
+    sizes: {
+        Small: 3.0,
+        Medium: 4.5,
+        Large: 6.0,
+    },
+};
